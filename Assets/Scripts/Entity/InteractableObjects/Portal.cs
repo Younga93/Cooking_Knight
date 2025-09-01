bf1689e0be8502c55a6c0db534bcda833679fe29
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-09-01 20:23:57 +0900

    [feat] 포탈 씬 전환 구현

---
+++ Assets/Scripts/Entity/InteractableObjects/Portal.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private SceneType sceneType;
+    private Coroutine _coroutine;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<Player>(out var player))
+        {
+            SceneChange();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        StopCoroutine(_coroutine);
+    }
+
+    private void SceneChange()
+    {
+        _coroutine = StartCoroutine(nameof(SceneChangeProcess));
+    }
+
+    private IEnumerator SceneChangeProcess()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneLoadManager.Instance.LoadScene(sceneType);
+    }
+}
