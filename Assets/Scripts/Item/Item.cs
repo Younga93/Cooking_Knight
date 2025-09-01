4bfc1435d818c69c3e4b1480661dbcb6bb5d11eb
ChanyTheAwesome <89036528+ChanyTheAwesome@users.noreply.github.com>
2025-09-01 20:07:55 +0900

    [fix] FullHD 환경에서 맵의 스케일이 맞지 않는 현상 수정

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -17,7 +17,7 @@ public class Item : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent<IItemCollector>(out var collector))
-        {
+        {//세상에나, IItemCollector가 필요없을 수도 있다. 단순히 Inventory 컴포넌트를 가져와서 넣어볼 수도 있겠다!
             collector.AddItem(itemData);
             _animator.SetTrigger(_collectedHash);
             Destroy(gameObject, 0.5f);
