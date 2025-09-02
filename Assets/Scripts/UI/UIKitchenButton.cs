87efd45720e5d00aae29ff6cc922da576bf80dd6
Younga <youngajin93@gmail.com>
2025-09-02 20:08:31 +0900

    [recover] 찬희님의 삭제된 파일들 복구해드림

--- Assets/Scripts/UI/UIKitchenButton.cs
+++ Assets/Scripts/UI/UIKitchenButton.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.UI;
 public class UIKitchenButton : UIBase
 {
-    [SerializeField] private Button button;
+    [SerializeField] private Button button; //
 
     private void OnEnable()
     {
