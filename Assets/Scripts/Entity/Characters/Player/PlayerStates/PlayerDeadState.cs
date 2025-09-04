6e1ada2009152bae4500fed36ca22146d5314cff
Younga <youngajin93@gmail.com>
2025-09-04 20:36:12 +0900

    [feat] 죽음 뒤 씬 전환

--- Assets/Scripts/Entity/Characters/Player/PlayerStates/PlayerDeadState.cs
+++ Assets/Scripts/Entity/Characters/Player/PlayerStates/PlayerDeadState.cs
@@ -1,9 +1,13 @@
+using System.Collections;
+
 public class PlayerDeadState : IPlayerState
 {
     public void EnterState(Player player)
     {
         player.PlayerAnimator.SetTrigger(AnimatorString.PlayerParameters.Dead);
         player.PlayerInputActions.Disable();
+        
+        player.OnResurrection();
     }
 
     public void UpdateState(Player player)
